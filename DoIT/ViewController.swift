c974c1da361a374b5a952d8d96a02b2060aa4940
Ima M Christian <imachris18@gmail.com>
2018-04-29 13:12:54 -0400

    Initial Commit

---
+++ DoIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoIT
+//
+//  Created by Ima M Christian on 4/29/18.
+//  Copyright © 2018 Ima M Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
